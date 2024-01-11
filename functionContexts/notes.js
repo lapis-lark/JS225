12404776523819a229353078d5d13f4a1cbd61c1
lapis <lapis.santuri@gmail.com>
2024-01-11 15:57:57 -0800

    function context first pass

---
+++ functionContexts/notes.js
@@ -0,0 +1,11 @@
+/*
+  var creates properties on the global object (in the global context)
+  let/const create variables that don't belong to any object!
+
+  review implicit/explicit execution context
+    i think explicit is just when call/apply/(bind?) used, not related to function/method invk
+
+
+*/
+
+
